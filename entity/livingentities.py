14adcab0ee0aa9e772c2ac38a72838a71559661a
staug <tangilemoine@gmail.com>
2017-11-05 19:11:32 +0100

    First NPC, as friendly entities

---
+++ entity/livingentities.py
@@ -0,0 +1,11 @@
+from entity.gameentity import GameEntity, WanderingAIEntity
+
+
+class FriendlyEntity(GameEntity):
+
+    def __init__(self, name, position):
+
+        image_ref = "HUMANOID_1"
+
+        GameEntity.__init__(self, name=name, pos=position, image_ref=image_ref, z_level=2, blocks=True,
+                            ai=WanderingAIEntity(speed=1))
